a6de2cc3e45072293ef8eb74c5cb9f96ad4a674d
Mark Wang <peiqi1122@gmail.com>
2016-06-01 02:42:34 -0400

    add loadvariantproperty table

--- DB/SAMPLEQUERIES.md
+++ DB/SAMPLEQUERIES.md
@@ -1,8 +1,19 @@
 
-__Get ORF sequence of transcript with refseq ID NM_001613__
+__Get ORF sequence of transcript with refseq ID (NM_001613)__
 
 ```sql
 SELECT ORFeome.CDS_ORFEOME_SEQ
 FROM ORFeome JOIN Transcript USING (ENTREZ_GENE_ID)
 WHERE (REFSEQ_ID = "NM_001613");
+```
+
+
+__Get a list of genes with a given disease (Hypospadias)__
+
+```sql
+SELECT Gene.HUGO_GENE_SYMBOL
+FROM Gene JOIN Transcript USING (ENTREZ_GENE_ID)
+          JOIN Variant USING(REFSEQ_ID)
+          JOIN Disease USING(VARIANT_ID)
+WHERE (Disease.DISEASE_NAME = "Hypospadias");
 ```
--- DB/loadData.py
+++ DB/loadData.py
@@ -21,8 +21,31 @@ MUT_ORFEOME_NT = "Mutation_HORFeome_8.1_NT"
 MUT_ORFEOME_AA = "Mutation_HORFeome_8.1_AA"
 CHR_COORDINATE_HG18 = "Chromosome_coordinate_hg18"
 PMID = "pmid"
+HGMD_ACCESSION = "HGMD_accession"
+MUT_HGMD_AA = "Mutation_HGMD_AA"
+HGMD_VARIANT_CLASS = "HGMD_variant_class"
 
+DISEASE_NAME = "Disease"
+INHERITANCE_PATTERN = 'Inheritance'
 
+DISORDER_PROBABILITY = "Disorder_probability"
+PFAM_PRESENT = "Pfam_present"
+IN_PFAM = "In_Pfam"
+IN_MOTIF = "In_motif"
+POLYPHEN_SCORE = "PolyPhen_2_score"
+POLYPHEN_CLASS = "PolyPhen_class"
+SOLVENT_ACCESSIBILITY = "Solvent_accessibility"
+CONSERVATION_INDEX = "Conservation_index"
+HYDROPHOBICITY_DECREASE = "Hydrophobicity_decrease"
+PROTEIN_CHEMICAL_INTERFACE = "Protein_chemical_interface"
+FOLDX_VALUE = "FoldX_value"
+CLINVAR_ID = "ClinVar_ID"
+CLINVAR_CLINICAL_SIGNIFICANCE = "ClinVar_clinical_significance"
+
+ENTRY_CLONE_PLATE = "Ph1Mut_PLA"
+ENTRY_CLONE_WELL = "Ph1Mut_POS"
+FLAG_CLONE_ID = "3xFLAG_clone_ID"
+FLAG_CHECK = "3xFLAG_BsrGI_check"
 
 
 class LoadData:
@@ -73,7 +96,7 @@ class LoadData:
 
     def checkEmptyString(self, listoftuples):
         """check for empty string in list[tuple] and return a dictionary
-        with index of tuples as keys"""
+        with index of tuples as keys"; mostly to see if a column has empty str"""
         dic = defaultdict(int)
         for item in listoftuples:
             for i, v in enumerate(item):
@@ -91,12 +114,13 @@ class LoadData:
         self.loadTranscriptTable()
         self.loadORFeomeTable()
         self.loadVariantTable()
+        self.loadVariantPropertyTable()
+        self.loadLocalCollectionTable()
 
     def loadGeneTable(self):
         inserts = self.subsetCols([ENTREZ_GENE_ID, HUGO_GENE_SYMBOL])
         sqlstr = """INSERT INTO Gene (ENTREZ_GENE_ID, HUGO_GENE_SYMBOL)
                 VALUES (%s, %s);"""
-        print(inserts)
         try:
             self.c.executemany(sqlstr, inserts)
             self.db.commit()
@@ -136,15 +160,17 @@ class LoadData:
         subsets = self.subsetCols([CCSB_MUTATION_ID, DBSNP_ID, MUT_HGVS_NT_ID,
                                     MUT_HGVS_AA_ID, MUT_ORFEOME_NT,
                                     MUT_ORFEOME_AA, CHR_COORDINATE_HG18,
-                                    PMID, REFSEQ_ID])
+                                    PMID, HGMD_ACCESSION, MUT_HGMD_AA,
+                                    HGMD_VARIANT_CLASS, REFSEQ_ID])
         inserts = self.replaceNullwithEmptyString(subsets)
         inserts = list(set([(t[0].split('_')[1], t[1],t[2], t[3], t[4], t[5],
-            t[6], t[7] if t[7] else 0, t[8].split(':')[0]) for t in inserts]))
+            t[6], t[7] if t[7] else 0, t[8], t[9], t[10], t[11].split(':')[0]) for t in inserts]))
         sqlstr = """INSERT INTO Variant(VARIANT_ID, CCSB_MUTATION_ID, DBSNP_ID, MUT_HGVS_NT_ID,
                                     MUT_HGVS_AA_ID, MUT_ORFEOME_NT,
                                     MUT_ORFEOME_AA, CHR_COORDINATE_HG18,
-                                    PMID, REFSEQ_ID)
-                VALUES (0, %s, NULLIF(%s, ''), %s, %s, %s, %s, %s, %s, %s);"""
+                                    PMID, HGMD_ACCESSION, MUT_HGMD_AA,
+                                    HGMD_VARIANT_CLASS, REFSEQ_ID)
+                VALUES (0, %s, %s, %s, %s, %s, %s, %s, %s, NULLIF(%s, ''), %s, %s, %s);"""
         try:
             self.c.executemany(sqlstr, inserts)
             self.db.commit()
@@ -154,15 +180,75 @@ class LoadData:
             self.db.rollback()
 
     def loadDiseaseTable(self):
-        subsets = self.subsetCols([DISEASE_NAME, INHERITANCE_PATTERN, REFSEQ_ID])
+        subsets = self.subsetCols([DISEASE_NAME, INHERITANCE_PATTERN, MUT_HGVS_NT_ID])
         inserts = self.replaceNullwithEmptyString(subsets)
-        inserts =
+        sqlstr = """INSERT INTO Disease (DISEASE_ID, DISEASE_NAME, INHERITANCE_PATTERN, VARIANT_ID)
+                VALUES (0, %s, %s, (SELECT VARIANT_ID FROM Variant WHERE MUT_HGVS_NT_ID = %s))
+                """
+        try:
+            self.c.executemany(sqlstr, inserts)
+            self.db.commit()
+        except MySQLdb.Error, e:
+            print 'insert to disease table failed'
+            raise e
+            self.db.rollback()
+
+    def loadVariantPropertyTable(self):
+        subsets = self.subsetCols([DISORDER_PROBABILITY, PFAM_PRESENT, IN_PFAM,
+                IN_MOTIF, POLYPHEN_SCORE, POLYPHEN_CLASS, SOLVENT_ACCESSIBILITY,
+                CONSERVATION_INDEX, HYDROPHOBICITY_DECREASE, PROTEIN_CHEMICAL_INTERFACE,
+                FOLDX_VALUE, CLINVAR_ID, CLINVAR_CLINICAL_SIGNIFICANCE, MUT_HGVS_NT_ID])
+        inserts = self.replaceNullwithEmptyString(subsets)
+        inserts = list(set([(t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7],
+            t[8], t[9], t[10], t[11] if t[11] else 0, t[12], t[13]) for t in inserts]))
+        # add nullif statement to any colname that is decimal or boolean (anything non-string)
+        # in this case NULL represent empty cell otherwise '' represeent empty cell
+        # for ids stored as INT, 0 represent empty cell
+        sqlstr = """INSERT INTO VariantProperty (VARIANT_PROPERTY_ID,
+                DISORDER_PROBABILITY, PFAM_PRESENT, IN_PFAM, IN_MOTIF,
+                POLYPHEN_SCORE, POLYPHEN_CLASS, SOLVENT_ACCESSIBILITY,
+                CONSERVATION_INDEX, HYDROPHOBICITY_DECREASE, PROTEIN_CHEMICAL_INTERFACE,
+                FOLDX_VALUE, CLINVAR_ID, CLINVAR_CLINICAL_SIGNIFICANCE, VARIANT_ID)
+                VALUES (0, NULLIF(%s, ''), NULLIF(%s, ''), NULLIF(%s, ''), NULLIF(%s, ''),
+                NULLIF(%s, ''), %s, %s, NULLIF(%s, ''), NULLIF(%s, ''), %s, NULLIF(%s, ''),
+                %s, %s,
+                (SELECT VARIANT_ID FROM Variant WHERE MUT_HGVS_NT_ID = %s))"""
+        try:
+            self.c.executemany(sqlstr, inserts)
+            self.db.commit()
+        except MySQLdb.Error, e:
+            print 'insert into VariantProperty table failed'
+            raise e
+            self.db.rollback()
+
+
+    def loadLocalCollectionTable(self):
+        subsets = self.subsetCols([ENTRY_CLONE_PLATE, ENTRY_CLONE_WELL,
+                                   FLAG_CLONE_ID,
+                                   FLAG_CHECK, MUT_HGVS_NT_ID])
+        inserts = self.replaceNullwithEmptyString(subsets)
+        # "".spilt('@')[0] returns "" but "".spilt('@')[1] returns index out of range
+        inserts = [(t[0], t[1], t[2].split('@')[0] if t[2] else '',
+                t[2].split('@')[1] if t[2] else '', t[3], t[4]) for t in inserts]
+        sqlstr = """INSERT INTO LocalCollection (LOCAL_COLLECTION_ID, ENTRY_CLONE_PLATE,
+                    ENTRY_CLONE_WELL, DESTINATION_CLONE_PLATE, DESTINATION_CLONE_WELL,
+                    FLAG_CHECK, VARIANT_ID)
+                    VALUES (0, %s, %s, %s, %s, %s,
+                    (SELECT VARIANT_ID FROM Variant WHERE MUT_HGVS_NT_ID = %s))"""
+        try:
+            self.c.executemany(sqlstr, inserts)
+            self.db.commit()
+        except MySQLdb.Error, e:
+            print 'insert into LocalCollection table failed'
+            raise e
+            self.db.rollback()
+
+    def
+
+
 
 if __name__ == "__main__":
     ld = LoadData()
     ld.importCSV("./origExcel/csvMutCollection.csv")
-    # ld.loadGeneTable()
-    # ld.loadTranscriptTable()
-    # ld.loadORFeomeTable()
-    # ld.loadVariantTable()
-    ld.loadTables()
+    # ld.loadTables()
+    ld.loadLocalCollectionTable()
