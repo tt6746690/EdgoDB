0ae690112d68852c52b9039d6b9b77237f71ed79
Mark Wang <peiqi1122@gmail.com>
2016-07-16 12:34:22 -0400

    adding ga

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ if (conf.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    console.err(err)
+    console.error(err)
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
